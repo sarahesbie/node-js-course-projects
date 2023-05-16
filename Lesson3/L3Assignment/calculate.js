e8839defbb73e08afc31152c175d6bef7c532349
SB <sarahbrown.slb@gmail.com>
2023-05-16 16:15:48 +0930

    setting up L3 assignment

---
+++ Lesson3/L3Assignment/calculate.js
@@ -0,0 +1,15 @@
+const calculate = {
+  sum: function (a, b) {
+    return a + b;
+  },
+
+  subtract: function (a, b) {
+    return a - b;
+  },
+
+  multiply: function (a, b) {
+    return a * b;
+  },
+};
+
+module.exports = calculate;
