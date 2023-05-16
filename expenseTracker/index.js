6d1b1912031f3d4f31d82273535a08667366c06a
SB <sarahbrown.slb@gmail.com>
2023-05-16 16:02:21 +0930

    using file system in expense tracker proj

--- expenseTracker/index.js
+++ expenseTracker/index.js
@@ -1,2 +1,36 @@
-require("dotenv").config();
-console.log(process.env.DB_HOST);
+// EXAMPLE ONE - read index.html file on port 8080 in browser
+
+// var http = require("http");
+// var fs = require("fs");
+
+// http
+//   .createServer(function (req, res) {
+//     fs.readFile("demo.html", function (err, data) {
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       return res.end();
+//     });
+//   })
+//   .listen(8080);
+
+//EXAMPLE TWO - append to file
+
+// var fs = require("fs");
+
+// fs.appendFile("message.txt", "Append the data in this file ", function (err) {
+//   if (err) throw err;
+//   console.log("Data successfully appended to file!");
+// });
+
+// EXAMPLE THREE - write to file (and overwrite)
+
+var fs = require("fs");
+
+fs.writeFile(
+  "message.txt",
+  "Write (and overwrite) the data in this file",
+  function (err) {
+    if (err) throw err;
+    console.log("Data successfully written to file");
+  }
+);
